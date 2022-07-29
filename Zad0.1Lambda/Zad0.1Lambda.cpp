16f1d18e311e17df8e2c419289a21dd74556668a
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-07-28 18:00:16 -0700

    Zad0.1Lambda

---
+++ Zad0.1Lambda/Zad0.1Lambda.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+void hello()
+{
+    std::cout << "Hello world function" << std::endl;
+}
+int addNums(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    auto helloLambda = []() {std::cout << "Hello world lambda" << std::endl; };
+    hello();
+    helloLambda();
+
+    auto addNumsLambda = [](int a, int b) -> int { return a + b; };
+    std::cout << addNums(5, 6) << std::endl;
+    std::cout << addNumsLambda(6,7) << std::endl;
+
+
+
+}
